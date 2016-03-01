80d025818a72445d152e39e82b790bf297d7d3fd
Touko Hallasmaa <touko.hallasmaa@outlook.com>
2016-03-01 15:33:18 +0200

    initial commit

---
+++ tilitUITests/tilitUITests.swift
@@ -0,0 +1,34 @@
+//
+//  tilitUITests.swift
+//  tilitUITests
+//
+//  Created by Touko Hallasmaa on 01.03.16.
+//  Copyright © 2016 Touko Hallasmaa. All rights reserved.
+//
+
+import XCTest
+
+class tilitUITests: XCTestCase {
+        
+    override func setUp() {
+        super.setUp()
+        
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+        XCUIApplication().launch()
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+}
